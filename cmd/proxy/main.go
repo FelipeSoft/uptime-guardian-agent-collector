61ba8a4a01401de618357deeb7c3da79c46d21c4
FelipeSoft <felipedecastrolima2@gmail.com>
2024-12-29 13:31:04 -0300

    [feat] Including environment file loading.

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -4,25 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"github.com/FelipeSoft/uptime-guardian-agent-collector/internal/infrastructure/icmp"
 	"github.com/FelipeSoft/uptime-guardian-agent-collector/internal/uptime"
 	pb "github.com/FelipeSoft/uptime-guardian-agent-collector/internal/uptime/v1/proto"
+	"github.com/joho/godotenv"
 	"golang.org/x/net/websocket"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	url := "http://localhost:9000"
-	origin := "http://localhost:50051"
-	ws, err := websocket.Dial(url, "", origin)
+	godotenv.Load("./../../.env")
+	websocketGatewayUrl := os.Getenv("WEBSOCKET_GATEWAY") + "?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm94eUlkIjoxLCJpYXQiOjE3MzU0ODkxNDIsImV4cCI6MTczNTQ4OTc0Mn0.DPnhBss05IK3hU4Ldg6Gl8C-ZcNjBgswaoaufZkFJhA"
+	origin := "http://192.168.0.4:50051"
+
+	ws, err := websocket.Dial(websocketGatewayUrl, "", origin)
+	if err != nil {
+		log.Fatalf("WebSocket Gateway connection failed: %s", err.Error())
+	}
 	defer ws.Close()
 
 	if err != nil {
-		log.Fatalf("Failed on establishing connection with Websocket Gateway.")
+		log.Fatalf("Failed on establishing connection with WebSocket Gateway.")
 	}
 
-	if err := icmp.AddICMPTask(1, &icmp.Task{IpAddr: "192.168.200.153"}); err != nil {
+	if err := icmp.AddICMPTask(1, &icmp.Task{IpAddr: "192.168.0.6"}); err != nil {
 		log.Fatalf("Failed to add task 1: %v", err)
 	}
 
@@ -32,20 +39,20 @@ func main() {
 		}
 	}()
 
-	go func () {
+	go func() {
 		for {
 			var message string
 			err := websocket.Message.Receive(ws, &message)
 			if err != nil {
-				log.Printf("Error on read message from Websocket Gateway: %s", err)
+				log.Printf("Error on read message from WebSocket Gateway: %s", err)
 			}
 			fmt.Println(message)
 		}
 	}()
 
-	// (Monitoramento Ativo) Receber do agente as informações
-	// (Monitoramento Passivo) -> se o hardware do Proxy for um pouco mais robusto ICMP com os hosts
-	lis, err := net.Listen("tcp", "192.168.200.154:50051")
+	// (Active Monitoring) Proxy receive agent informations
+	// (Passive Monitoring) -> If the proxy hardware is robust, could make ICMP calls with hosts (installed agents)
+	lis, err := net.Listen("tcp", os.Getenv("PROXY_SERVER"))
 	if err != nil {
 		log.Fatalf("listening to gRPC failed with: %s", err.Error())
 	}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.4
 require google.golang.org/grpc v1.69.2
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
