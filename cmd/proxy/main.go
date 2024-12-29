2107092373dc2036a584b04259d1de1a6e3eade5
FelipeSoft <felipedecastrolima2@gmail.com>
2024-12-29 15:37:05 -0300

    [feat] Including the active authentication from Proxy to WebSocket Gateway.

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,69 +1,77 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
+	// "fmt"
+	// "log"
+	// "net"
 	"os"
-
-	"github.com/FelipeSoft/uptime-guardian-agent-collector/internal/infrastructure/icmp"
-	"github.com/FelipeSoft/uptime-guardian-agent-collector/internal/uptime"
-	pb "github.com/FelipeSoft/uptime-guardian-agent-collector/internal/uptime/v1/proto"
+	// "github.com/FelipeSoft/uptime-guardian-agent-collector/internal/infrastructure/icmp"
+	// "github.com/FelipeSoft/uptime-guardian-agent-collector/internal/uptime"
+	// pb "github.com/FelipeSoft/uptime-guardian-agent-collector/internal/uptime/v1/proto"
+	"github.com/FelipeSoft/uptime-guardian-agent-collector/internal/usecase"
 	"github.com/joho/godotenv"
-	"golang.org/x/net/websocket"
-	"google.golang.org/grpc"
+	// "golang.org/x/net/websocket"
+	// "google.golang.org/grpc"
 )
 
 func main() {
 	godotenv.Load("./../../.env")
-	websocketGatewayUrl := os.Getenv("WEBSOCKET_GATEWAY") + "?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm94eUlkIjoxLCJpYXQiOjE3MzU0ODkxNDIsImV4cCI6MTczNTQ4OTc0Mn0.DPnhBss05IK3hU4Ldg6Gl8C-ZcNjBgswaoaufZkFJhA"
-	origin := "http://192.168.0.4:50051"
+	// websocketGatewayWsUrl := os.Getenv("WEBSOCKET_GATEWAY_WS")
 
-	ws, err := websocket.Dial(websocketGatewayUrl, "", origin)
-	if err != nil {
-		log.Fatalf("WebSocket Gateway connection failed: %s", err.Error())
-	}
-	defer ws.Close()
+	websocketGatewayHttpUrl := os.Getenv("WEBSOCKET_GATEWAY_HTTP")
+	// proxyUrl := "http://192.168.0.4:50051"
 
-	if err != nil {
-		log.Fatalf("Failed on establishing connection with WebSocket Gateway.")
-	}
+	usecase.AuthProxy(usecase.Input{
+		Host: websocketGatewayHttpUrl,
+		Protocol: "http",
+		Path: "/auth/proxy",
+	})
 
-	if err := icmp.AddICMPTask(1, &icmp.Task{IpAddr: "192.168.0.6"}); err != nil {
-		log.Fatalf("Failed to add task 1: %v", err)
-	}
+	// ws, err := websocket.Dial(websocketGatewayWsUrl, "", proxyUrl)
+	// if err != nil {
+	// 	log.Fatalf("WebSocket Gateway connection failed: %s", err.Error())
+	// }
+	// defer ws.Close()
 
-	go func() {
-		if err := icmp.ICMPScheduler(); err != nil {
-			log.Fatalf("Scheduler error: %v", err)
-		}
-	}()
+	// if err != nil {
+	// 	log.Fatalf("Failed on establishing connection with WebSocket Gateway.")
+	// }
 
-	go func() {
-		for {
-			var message string
-			err := websocket.Message.Receive(ws, &message)
-			if err != nil {
-				log.Printf("Error on read message from WebSocket Gateway: %s", err)
-			}
-			fmt.Println(message)
-		}
-	}()
+	// if err := icmp.AddICMPTask(1, &icmp.Task{IpAddr: "192.168.0.6"}); err != nil {
+	// 	log.Fatalf("Failed to add task 1: %v", err)
+	// }
 
-	// (Active Monitoring) Proxy receive agent informations
-	// (Passive Monitoring) -> If the proxy hardware is robust, could make ICMP calls with hosts (installed agents)
-	lis, err := net.Listen("tcp", os.Getenv("PROXY_SERVER"))
-	if err != nil {
-		log.Fatalf("listening to gRPC failed with: %s", err.Error())
-	}
-	defer lis.Close()
-	grpcServer := grpc.NewServer()
-	pb.RegisterUptimeServiceServer(grpcServer, &uptime.UptimeService{})
+	// go func() {
+	// 	if err := icmp.ICMPScheduler(); err != nil {
+	// 		log.Fatalf("Scheduler error: %v", err)
+	// 	}
+	// }()
 
-	log.Println("gRPC server is running on port 50051")
-	if err = grpcServer.Serve(lis); err != nil {
-		log.Fatalf("listening on TCP 50051 gRPC port failed: %s", err.Error())
-	}
+	// go func() {
+	// 	for {
+	// 		var message string
+	// 		err := websocket.Message.Receive(ws, &message)
+	// 		if err != nil {
+	// 			log.Printf("Error on read message from WebSocket Gateway: %s", err)
+	// 		}
+	// 		fmt.Println(message)
+	// 	}
+	// }()
 
-	select {}
+	// // (Active Monitoring) Proxy receive agent informations
+	// // (Passive Monitoring) -> If the proxy hardware is robust, could make ICMP calls with hosts (installed agents)
+	// lis, err := net.Listen("tcp", os.Getenv("PROXY_SERVER"))
+	// if err != nil {
+	// 	log.Fatalf("listening to gRPC failed with: %s", err.Error())
+	// }
+	// defer lis.Close()
+	// grpcServer := grpc.NewServer()
+	// pb.RegisterUptimeServiceServer(grpcServer, &uptime.UptimeService{})
+
+	// log.Println("gRPC server is running on port 50051")
+	// if err = grpcServer.Serve(lis); err != nil {
+	// 	log.Fatalf("listening on TCP 50051 gRPC port failed: %s", err.Error())
+	// }
+
+	// select {}
 }
---
+++ internal/usecase/auth_proxy.go
@@ -0,0 +1,75 @@
+package usecase
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Input struct {
+	Host     string
+	Protocol string
+	Path     string
+}
+
+type ProxyAuthDataBody struct {
+	ProxyId       int    `json:"proxyId"`
+	ProxyPassword string `json:"proxyPassword"`
+}
+
+type Output struct {
+	Token string `json:"token"`
+}
+
+func AuthProxy(input Input) (*Output, error) {
+	godotenv.Load("./../../.env")
+	fmt.Println(os.Getenv("PROXY_ID"))
+	client := http.Client{
+		Transport: http.DefaultTransport,
+	}
+	u := url.URL{
+		Scheme: input.Protocol,
+		Host:   input.Host,
+		Path:   input.Path,
+	}
+	proxyId, err := strconv.Atoi(os.Getenv("PROXY_ID"))
+	proxyPassword := os.Getenv("PROXY_PASSWORD")
+	if err != nil {
+		log.Printf("error on proxyId int parsing: %s", err.Error())
+	}
+	body, err := json.Marshal(&ProxyAuthDataBody{ProxyId: proxyId, ProxyPassword: proxyPassword})
+	fmt.Println(string(body))
+	if err != nil {
+		log.Printf("error on body marshal: %s", err.Error())
+	}
+	buff := bytes.NewBuffer(body)
+	req, err := http.NewRequest("POST", u.String(), buff)
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Printf("error on auth proxy request: %s", err.Error())
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("error on auth proxy response: %s", err.Error())
+	}
+	defer res.Body.Close()
+
+	var output *Output
+	parsedResponse, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("error on auth proxy body decode: %s", err.Error())
+	}
+	err = json.Unmarshal(parsedResponse, &output)
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
+}
